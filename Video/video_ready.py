14931a56b57eb81a7e37b317ed2e304c7f7b6df3
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2022-02-06 22:49:20 +0800

    :sparkles: add name param for start api

--- Video/video_ready.py
+++ Video/video_ready.py
@@ -5,6 +5,7 @@ import psutil
 import subprocess
 import os
 import signal
+from urllib.parse import urlparse, parse_qs
 
 video_ready_port = int(environ.get('VIDEO_READY_PORT', 9000))
 
@@ -13,13 +14,15 @@ class Handler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            if self.path == "/start":
-                Handler.process = subprocess.Popen("/opt/bin/video.sh", stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+            if self.path.startswith("/start"):
+                query_components = parse_qs(urlparse(self.path).query)
+                name = query_components["name"][0] if 'name' in query_components else "video"
+                Handler.process = subprocess.Popen("/opt/bin/video.sh " + name, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
                 self.send_response(200)
                 self.end_headers()
                 self.wfile.write(json.dumps({'status': 'video recording started', 'pid': Handler.process.pid}).encode('utf-8'))
                 return
-            if self.path == "/end":
+            if self.path.startswith("/end"):
                 os.killpg(os.getpgid(Handler.process.pid), signal.SIGTERM)
                 self.send_response(200)
                 self.end_headers()
