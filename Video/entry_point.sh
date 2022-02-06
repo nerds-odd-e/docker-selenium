032d60a30cb047c1407968d6517aa8fe18b44cfe
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2022-02-06 21:46:48 +0800

    :sparkles: add end video api

--- Video/entry_point.sh
+++ Video/entry_point.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-/usr/bin/supervisord --configuration /etc/supervisord.conf &
+python3 /opt/bin/video_ready.py &
 
-SUPERVISOR_PID=$!
-
-function shutdown {
-    echo "Trapped SIGTERM/SIGINT/x so shutting down supervisord..."
-    kill -s SIGTERM ${SUPERVISOR_PID}
-    wait ${SUPERVISOR_PID}
-    echo "Shutdown complete"
-}
-
-trap shutdown SIGTERM SIGINT
-wait ${SUPERVISOR_PID}
+tail -f /dev/null
--- Video/video_ready.py
+++ Video/video_ready.py
@@ -2,25 +2,33 @@ from http.server import BaseHTTPRequestHandler,HTTPServer
 from os import environ
 import json
 import psutil
-from subprocess import Popen
+import subprocess
+import os
+import signal
 
 video_ready_port = int(environ.get('VIDEO_READY_PORT', 9000))
 
 class Handler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        video_ready = "ffmpeg" in (p.name().lower() for p in psutil.process_iter())
-        response_code = 200 if video_ready else 404
-        response_text = "ready" if video_ready else "not ready"
-        self.send_response(response_code)
-        self.end_headers()
-        self.wfile.write(json.dumps({'status': response_text}).encode('utf-8'))
+    process: None
 
     def do_POST(self):
-        Popen("/opt/bin/video.sh")
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(json.dumps({'status': 'video recording started'}).encode('utf-8'))
+        try:
+            if self.path == "/start":
+                Handler.process = subprocess.Popen("/opt/bin/video.sh", stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(json.dumps({'status': 'video recording started', 'pid': Handler.process.pid}).encode('utf-8'))
+                return
+            if self.path == "/end":
+                os.killpg(os.getpgid(Handler.process.pid), signal.SIGTERM)
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(json.dumps({'status': 'video recording ended'}).encode('utf-8'))
+                return
+        except Exception as e:
+            self.send_response(500)
+            self.end_headers()
+            self.wfile.write(json.dumps({'exception': str(e)}).encode('utf-8'))
 
 httpd = HTTPServer( ('0.0.0.0', video_ready_port), Handler )
 httpd.serve_forever()
