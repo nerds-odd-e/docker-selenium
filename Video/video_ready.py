826ce5082adedfe4391f1563cf32ab5ae7b95c9a
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2022-02-11 13:38:25 +0800

    :bug: fix the issue scenario name cause video record failed

--- Video/video_ready.py
+++ Video/video_ready.py
@@ -1,13 +1,13 @@
-from http.server import BaseHTTPRequestHandler,HTTPServer
-from os import environ
 import json
-import psutil
-import subprocess
 import os
 import signal
+import subprocess
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from os import environ
 from urllib.parse import urlparse, parse_qs
 
 video_ready_port = int(environ.get('VIDEO_READY_PORT', 9000))
+
 
 class Handler(BaseHTTPRequestHandler):
     process: None
@@ -17,21 +17,28 @@ class Handler(BaseHTTPRequestHandler):
             if self.path.startswith("/start"):
                 query_components = parse_qs(urlparse(self.path).query)
                 name = query_components["name"][0] if 'name' in query_components else "video"
-                Handler.process = subprocess.Popen("/opt/bin/video.sh " + name, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+                Handler.process = subprocess.Popen("/opt/bin/video.sh '" + name + "'", stdout=subprocess.PIPE,
+                                                   shell=True,
+                                                   preexec_fn=os.setsid)
                 self.send_response(200)
+                self.send_header('Content-type', 'application/json')
                 self.end_headers()
-                self.wfile.write(json.dumps({'status': 'video recording started', 'pid': Handler.process.pid}).encode('utf-8'))
+                self.wfile.write(
+                    json.dumps({'status': 'video recording started', 'pid': Handler.process.pid}).encode('utf-8'))
                 return
             if self.path.startswith("/end"):
                 os.killpg(os.getpgid(Handler.process.pid), signal.SIGTERM)
                 self.send_response(200)
+                self.send_header('Content-type', 'application/json')
                 self.end_headers()
                 self.wfile.write(json.dumps({'status': 'video recording ended'}).encode('utf-8'))
                 return
         except Exception as e:
             self.send_response(500)
+            self.send_header('Content-type', 'application/json')
             self.end_headers()
             self.wfile.write(json.dumps({'exception': str(e)}).encode('utf-8'))
 
-httpd = HTTPServer( ('0.0.0.0', video_ready_port), Handler )
+
+httpd = HTTPServer(('0.0.0.0', video_ready_port), Handler)
 httpd.serve_forever()
