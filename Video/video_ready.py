95a71e184fd3c44354c749264cb4c292485aed62
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2022-02-13 13:16:05 +0800

    :recycle: refactor video record server

--- Video/video_ready.py
+++ Video/video_ready.py
@@ -16,32 +16,33 @@ class Handler(BaseHTTPRequestHandler):
         try:
             if self.path.startswith("/start"):
                 if hasattr(Handler, 'process') and Handler.process is not None:
-                    os.killpg(os.getpgid(Handler.process.pid), signal.SIGTERM)
-                    Handler.process = None
-                query_components = parse_qs(urlparse(self.path).query)
-                name = query_components["name"][0] if 'name' in query_components else "video"
-                Handler.process = subprocess.Popen("/opt/bin/video.sh '" + name + "'", stdout=subprocess.PIPE,
-                                                   shell=True,
-                                                   preexec_fn=os.setsid)
-                self.send_response(200)
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                self.wfile.write(
-                    json.dumps({'status': 'video recording started', 'pid': Handler.process.pid}).encode('utf-8'))
+                    self.end_recording()
+                self.start_recording()
+                self.respond_with_body(200, {'status': 'video recording started', 'pid': Handler.process.pid})
                 return
             if self.path.startswith("/end"):
-                os.killpg(os.getpgid(Handler.process.pid), signal.SIGTERM)
-                Handler.process = None
-                self.send_response(200)
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                self.wfile.write(json.dumps({'status': 'video recording ended'}).encode('utf-8'))
+                self.end_recording()
+                self.respond_with_body(200, {'status': 'video recording ended'})
                 return
         except Exception as e:
-            self.send_response(500)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            self.wfile.write(json.dumps({'exception': str(e)}).encode('utf-8'))
+            self.respond_with_body(500, {'exception': str(e)})
+
+    def start_recording(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        name = query_components["name"][0] if 'name' in query_components else "video"
+        Handler.process = subprocess.Popen("/opt/bin/video.sh '" + name + "'", stdout=subprocess.PIPE,
+                                           shell=True,
+                                           preexec_fn=os.setsid)
+
+    def end_recording(self):
+        os.killpg(os.getpgid(Handler.process.pid), signal.SIGTERM)
+        Handler.process = None
+
+    def respond_with_body(self, code, body):
+        self.send_response(code)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(body).encode('utf-8'))
 
 
 httpd = HTTPServer(('0.0.0.0', video_ready_port), Handler)
